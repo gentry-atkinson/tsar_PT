9412a0696304a5c2ef11262ef1b148ab035a7b82
gentry <gma23@txstate.edu>
2022-03-20 10:10:09 -0500

    filling in some frontend stuff

--- src/frontend.py
+++ src/frontend.py
@@ -1,12 +1,25 @@
 import pygame as pg
 
+screen = None
+running = True
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+
+
 def setup():
     print('starting')
+    pg.init()
+    screen = pg.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
 
 def main_loop():
-    pass
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            print('exit')
+            running = False
 
 if __name__ == '__main__':
     setup()
-    while(True):
+    while(running):
         main_loop()
+    pg.quit()
